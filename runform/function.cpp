786678c5fffb89bc72cf8985ccf205e6340d9828
Axel K. Reinhold <github@freakout.de>
2024-07-28 11:56:18 +0200

    trgnxtitem

--- runform/function.cpp
+++ runform/function.cpp
@@ -94,8 +94,8 @@ return 0;
 
 /* NAVIGATION */
 int Function::next_item() {
-  
-  return fmove(0,  1);
+  if (!trigger(TRT_NEXTITEM)) fmove(0, 1);
+  return 0;
 }
 int Function::previous_item()   { return fmove(0, -1); }
 int Function::next_record()     { return fmover(1);    }
@@ -229,7 +229,7 @@ return 0;
 
 int Function::trigger(int tid) {
 int i, s;
-s = -1;
+s = 0;
 if (tid >= 100) tid += CF.field_id * 1000;
 for (i=0; i<F(numtrigger); i++) if (F(r[i]).triggerid() == tid) s = F(r[i]).jsexec();
 return s;
