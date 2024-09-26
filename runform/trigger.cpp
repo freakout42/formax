949382023455fb3feef3242df10a2fda0c0db49c
Axel K. Reinhold <github@freakout.de>
2024-09-26 19:40:31 +0200

    return field value from selector

--- runform/trigger.cpp
+++ runform/trigger.cpp
@@ -9,15 +9,17 @@
 static char engine[HUGSIZE];
 static struct js *javascript = NULL;
 
-/* char *js_getstr(struct js *js, jsval_t value, size_t *len) */
+/* char *js_getstr(struct js *js, jsval_t value, size_t *len); */
+/* jsval_t js_mkstr(struct js *, const void *, size_t); */
+
 jsval_t j_snub(struct js *js, jsval_t *args, int nargs) {
-char fieldindex[6];
+char *fieldvalue;
 char *selector;
 selector = js_getstr(js, args[0], NULL);
-letf(t(fieldindex), "%5d", F(qfield)(selector));
+fieldvalue = *F(l)[F(qfield)(selector)].valuep();
 /*return js_mknum(js_getnum(args[0]));*/
 /*return js_mkstr(js, js_getstr(js, args[0], NULL), SMLSIZE);*/
-return js_mkstr(js, fieldindex, sizeof(fieldindex));
+return js_mkstr(js, fieldvalue, SMLSIZE);
 }
 
 #define JSEXT(func) jsval_t j_ ## func (struct js *js, jsval_t *args, int nargs);
